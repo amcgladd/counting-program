b3d01c4aff43c50e785c00609576714d62fda14b
AJ Mcgladdery and Dustin Starke <dustin@>
2018-08-14 10:21:52 -0700

    include parameters for user input that does not satisfy terms

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,30 @@ $(document).ready(function() {
     var outputs = [];
     var currentTotal = 1;
 
+    /*handles empty input*/
     if (!countToInput || !countByInput) {
-  alert("You gotta enter a number, derp!");
-};
+      alert("You gotta enter a number, derp!");
+    };
+console.log(countToInput, countByInput, outputs, currentTotal);
+    /*handles ratio issue*/
     if (countToInput < countByInput) {
         alert("Try harder. You can't count to a number with more than whatcha got.");
         }
 
-    /*handling negative numbers*/
-    if (countToInput < 0) {
+    /*handling negative numbers- bug: doesn't handle counting negatively with positive numbers
+    if (countToInput < 0 ) {
       for(var index = -1; currentTotal > countToInput; index -= 1) {
         currentTotal = countByInput * index;
         outputs.push(currentTotal);
         console.log(outputs);
     }
     }
+    */
+
+    if (countToInput < 0 || countByInput < 0) {
+        alert("Please stay positive. No negative numbers.");
+        }
+
 
     /*handling postive numbers*/
     for(var index = 1; currentTotal < countToInput; index += 1) {
@@ -30,6 +39,6 @@ $(document).ready(function() {
       console.log(outputs);
     }
 
-    $("#output").text(outputs);`
+    $("#output").text(outputs);
   });
 });
